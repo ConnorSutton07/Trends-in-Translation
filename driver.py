ab492f9aff8d3f39504c09ddde7284af7141a42b
Connor <connorsutton07@gmail.com>
2021-12-26 17:37:43 -0600

    reworked UI

--- driver.py
+++ driver.py
@@ -10,6 +10,7 @@ from core import settings
 from core import ui
 from core.translation import Translation
 from adjustText import adjust_text
+from tqdm import tqdm
 
 class Driver:
     def __init__(self) -> None:
@@ -17,34 +18,67 @@ class Driver:
         self.paths["current"]    = os.getcwd()
         self.paths["texts"]      = os.path.join(self.paths["current"], "texts")
         self.paths["Persai"]     = os.path.join(self.paths["texts"], "Persai")
-        self.paths["Gallico"]    = os.path.join(self.paths["texts"], "De Bello Gallico")
+        self.paths["Gallicum"]   = os.path.join(self.paths["texts"], "Gallicum")
         self.paths["figures"]    = os.path.join(self.paths["current"], "figures")
         self.paths["sentiment"]  = os.path.join(self.paths["figures"], "sentiment")
         self.paths["wordclouds"] = os.path.join(self.paths["figures"], "wordclouds")
         self.paths["embeddings"] = os.path.join(self.paths["figures"], "embeddings")
 
-        self.modes = {
-            'sentiment' :          self.sentiment,
-            'wordclouds':          self.wordclouds,
-            'embeddings':          self.embeddings,
-            'embeddings-animated': self.animated_embeddings
-        }
+        self.texts = [
+            ("Commentarii de Bello Gallico", "Gallicum"),
+            ("Persai", "Persai")
+        ]
 
-        args = self.parse_args()
-        self.mode = args.mode
+        self.modes = [
+            ("Create Embeddings", self.embeddings),
+            ("Animate Embeddings", self.animated_embeddings),
+            ("Generate Wordclouds", self.wordclouds),
+            ("Back", self.run)
+        ]
 
-    def run(self) -> None:
-        with open(os.path.join(self.paths["Gallico"], "info.json")) as infile:
+        print()
+        print("     +" + "-"*21 + "+")
+        print("      TRENDS IN TRANSLATION")
+        print("     +" + "-"*21 + "+")
+        print()
+
+    def run(self, _ = None, __ = None) -> None:
+        #ui.runModes(modes)
+        print("here")
+        translation_path = self.select_translation()
+        if translation_path is None:
+            return
+        with open(os.path.join(self.paths[translation_path], "info.json")) as infile:
             data = json.load(infile)
         translations = []
         for info in data:
-            translations.append(Translation(info, self.paths["Gallico"]))
+            translations.append(Translation(info, self.paths[translation_path]))
         translations.sort(key = lambda t: t.year)
-        self.modes[self.mode](translations)
+        self.select_mode()(translations, translation_path)
+
+    def select_mode(self) -> callable:
+        num_modes = len(self.modes)
+        msg = "Analysis Method:"
+        for i, mode in enumerate(self.modes, start=1):
+            msg += f"\n   {i}) {mode[0]}"
+        index = ui.getValidInput(msg, dtype=int, valid=range(1, num_modes + 1)) - 1
+        return self.modes[index][1]
+
+    def select_translation(self) -> str:
+        num_texts = len(self.texts)
+        msg = "Select Source Text:"
+        for i, text in enumerate(self.texts, start=1):
+            msg += f"\n   {i}) {text[0]}"
+        back_index = num_texts + 1
+        msg += f"\n   {back_index}) Exit"
+        index = ui.getValidInput(msg, dtype=int, valid=range(1, num_texts + 2)) - 1
+        if index != back_index - 1:
+            return self.texts[index][1]
+        return None
         
     def sentiment(self, translations: list) -> None:
         fig = plt.figure()
-        title = "Aeschylus' Persians | Sentiment Over Time"
+        title = "Sentiment Over Time"
         for t in translations:
             t.print_info()
             text = t.get_delimited_text()
@@ -62,24 +96,26 @@ class Driver:
         plt.savefig(os.path.join(self.paths["sentiment"], "sentiment_comparison.png"), dpi = 100)
         plt.show()
 
-    def wordclouds(self, translations: list) -> None:
+    def wordclouds(self, translations: list, text_path: str) -> None:
+        print("Generating wordclouds for the following translations:")
         for t in translations:
             t.print_info()
             plt.figure()
             plt.imshow(t.generate_wordcloud(stopwords = settings.stopwords, size = (400, 400)), interpolation = 'bilinear', cmap = 'Paired')
             plt.axis('off')
-            plt.savefig(os.path.join(self.paths["wordclouds"], "wordcloud_" + t.lastname + ".jpg"))
+            plt.savefig(os.path.join(self.paths["figures"], text_path, "wordclouds", f"wordcloud_{t.lastname}.jpg"))
             #plt.show()
 
-    def embeddings(self, translations: list, printing = False) -> None:
-        for t in translations:
+    def embeddings(self, translations: list, text_path: str, printing: bool = True) -> None:
+        print("Creating embeddings...")
+        for t in tqdm(translations):
             text = t.get_delimited_text()
             text = analysis.preprocess_text(text, stopwords = settings.stopwords, replacements = settings.replacements)
             corpus = []
             for section in text:
                 corpus.append(section.split(' '))
 
-            similar_words, all_words, pcs, explained_variance = analysis.analyze_embeddings(corpus, settings.key_words['gallico'])
+            similar_words, all_words, pcs, explained_variance = analysis.analyze_embeddings(corpus, settings.key_words[text_path])
             all_words, indices = np.unique(all_words, return_index=True)
             pcs = pcs[indices]
 
@@ -96,9 +132,10 @@ class Driver:
                 plt.xlabel("PC1 | " + "{:.2%}".format(explained_variance[0]))
                 plt.ylabel("PC2 | " + "{:.2%}".format(explained_variance[1]))
                 plt.title(f"Translation: {t.get_info()}") 
-                plt.savefig(os.path.join(self.paths["embeddings"], "embeddings_" + t.lastname + ".jpg"), dpi=300)
+                plt.savefig(os.path.join(self.paths["figures"], text_path, "embeddings", "embeddings_" + t.lastname + ".jpg"), dpi=300)
 
             if printing:
+                print()
                 t.print_info()
                 for k,v in similar_words.items():
                     print(f"{k}: {v}")
@@ -167,9 +204,3 @@ class Driver:
                                        fargs = (N, embedding_info),
                                        repeat = True)
         anim.save(os.path.join(self.paths["embeddings"], "embeddings_animated.gif"))
-
-    def parse_args(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('mode', choices = self.modes.keys(), help = 'determines which method of analysis to use')
-        args = parser.parse_args()
-        return args
